35da2953517a87dd34b2a2132dd93ac4ed622ec7
zeel <zeel02@gmail.com>
2015-12-13 23:07:10 -0500

    Added Hello World

---
+++ Hello World/Hello World.cpp
@@ -0,0 +1,13 @@
+// Hello World.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "Hello, World." << std::endl;
+    return 0;
+}
+
