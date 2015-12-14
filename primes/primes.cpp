de470213decac1d2de118238dc05d8d4d53b32d2
zeel <zeel02@gmail.com>
2015-12-14 01:20:42 -0500

    Completed Project 2

--- primes/primes.cpp
+++ primes/primes.cpp
@@ -22,13 +22,44 @@ bool is_prime(int n)
 
 int main()
 {
-	for (int i = 2; i < 100000000; i++)
-	{
-		if (is_prime(i))
-		{
-			std::cout << i << std::endl;
-		}
+	int maxThreads = 16;
+	int target = 100000000;
+
+	std::vector<cs477::thread> threads;
+	threads.reserve(maxThreads);
+
+	cs477::vector<int>* primes = new cs477::vector<int>[maxThreads];
+
+	int chunkSize = target / maxThreads;
+	primes[0].push_back(2);
+
+	auto start = now();
+
+	for (int i = 0; i < maxThreads; i++) {
+		auto t = cs477::create_thread([&primes, chunkSize, i] {
+			int start = chunkSize * i + 1;
+			for (int n = start; n < start + chunkSize; n += 2) {
+				if (is_prime(n)) primes[i].push_back(n);
+			}
+		});
+
+		threads.push_back(t);
 	}
+
+	for (auto &t : threads) {
+		cs477::join(t);
+	}
+
+	auto time = to_seconds(start, now());
+
+	std::cout << "Total Time: " << time << std::endl;
+
+	for (int i = 0; i < maxThreads; i++) {
+		for (auto t : primes[i]) std::cout << t << std::endl;
+	}
+
+	std::cout << "Total Time: " << time << std::endl;
+
     return 0;
 }
 
