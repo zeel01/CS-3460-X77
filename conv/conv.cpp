1935e881f90218c51b7825e9601a1550429fe4d4
zeel <zeel02@gmail.com>
2015-12-15 03:22:40 -0500

    Completed Project 3

--- conv/conv.cpp
+++ conv/conv.cpp
@@ -8,57 +8,71 @@
 
 
 
-void conv(matrix &x, const matrix &k)
+void conv(matrix &bmp, const matrix &kernal)
 {
 	matrix y;
-	y.create(x.rows + k.rows, x.cols + k.cols);
+	y.create(bmp.rows + kernal.rows, bmp.cols + kernal.cols);
 
-	for (unsigned row = 0; row < x.rows; row++)
+	for (unsigned row = 0; row < bmp.rows; row++)
 	{
-		for (unsigned col = 0; col < x.cols; col++)
+		for (unsigned col = 0; col < bmp.cols; col++)
 		{
-			auto yrow = row + k.rows / 2;
-			auto ycol = col + k.cols / 2;
-			y(yrow, ycol) = x(row, col);
+			auto yrow = row + kernal.rows / 2;
+			auto ycol = col + kernal.cols / 2;
+			y(yrow, ycol) = bmp(row, col);
 		}
 	}
 
 	// Compute sum of k
 	int weight = 0;
-	for (unsigned row = 0; row < k.rows; row++)
+	for (unsigned row = 0; row < kernal.rows; row++)
 	{
-		for (unsigned col = 0; col < k.cols; col++)
+		for (unsigned col = 0; col < kernal.cols; col++)
 		{
-			weight += k(row, col);
+			weight += kernal(row, col);
 		}
 	}
 
+	int maxThreads = 16;
+	std::vector<cs477::thread> threads;
+	threads.reserve(maxThreads);
+	
+	//If the height of bmp is not divisible by maxThreads, divide by one less so our chunks are big enough to deal with the whole image
+	int chunkSize = bmp.rows / (maxThreads - !(bmp.rows % maxThreads == 0)); 
+	
 	// Do the convolution
-	for (unsigned row = 0; row < x.rows; row++)
-	{
-		for (unsigned col = 0; col < x.cols; col++)
-		{
-			int t = 0;
+	for (unsigned c = 0; c < maxThreads; c++) {
+		auto t = cs477::create_thread([&bmp, &kernal, &y, c, chunkSize, weight]() {
+			int start = chunkSize * c;
+			int end = start + chunkSize;
 
-			auto yrow = row;
-			for (int krow = k.rows - 1; krow >= 0; krow--, yrow++)
-			{
-				auto ycol = col;
-				for (int kcol = k.cols - 1; kcol >= 0; kcol--, ycol++)
-				{
-					t += y(yrow, ycol) * k(krow, kcol);
+			for (unsigned row = start; row < end && row < bmp.rows; row++) {
+				for (unsigned col = 0; col < bmp.cols; col++) {
+					int t = 0;
+
+					auto yrow = row;
+					for (int krow = kernal.rows - 1; krow >= 0; krow--, yrow++) {
+						auto ycol = col;
+						for (int kcol = kernal.cols - 1; kcol >= 0; kcol--, ycol++) {
+							t += y(yrow, ycol) * kernal(krow, kcol);
+						}
+					}
+
+					if (weight != 0) {
+						t /= weight;
+					}
+
+					bmp(row, col) = t;
 				}
 			}
+		});
 
-			if (weight != 0)
-			{
-				t /= weight;
-			}
-
-			x(row, col) = t;
-		}
+		threads.push_back(t);
 	}
 
+	for (auto &t : threads) {
+		cs477::join(t);
+	}
 
 }
 
@@ -104,10 +118,10 @@ matrix binomial(int n)
 
 int main()
 {
-	auto bmp = load_image("C:\\Dev\\2015\\90.jpg");
+	auto bmp = load_image("kittens.png");
 	auto orig = bmp;
 
-	matrix kernel = binomial(1);
+	matrix kernel = binomial(11);
 
 	auto start = now();
 	conv(bmp, kernel);
@@ -115,7 +129,7 @@ int main()
 	
 	printf("%g\n", to_seconds(start, stop));
 
-	save_png(bmp, "C:\\Dev\\2015\\test.png");
+	save_png(bmp, "kittens.bmp");
 
     return 0;
 }
